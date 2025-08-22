2480043f37ad8feb972d3a0338e24162eb1631ab
hulklee1 <hulklee@gmail.com>
2025-08-22 15:43:22 +0900

    1

--- test1.c
+++ test1.c
@@ -10,7 +10,6 @@ int main(void)
 	printf("\n\nMy age : %d \n", age);
 	printf("%d is my point \n", score);
 	printf("Good \nmorning \neverybody\n");
-	//printf("My age : %d \n%d is my point \nGood \nmorning \neverybody\n", 20 ,100);
 
 	return 0;
 }
