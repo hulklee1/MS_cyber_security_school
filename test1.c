4824d6119b3ebc15606e30e4b1d6c14a9ebacf34
hulklee1 <hulklee@gmail.com>
2025-08-22 15:37:38 +0900

    1

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age, score;  // 정수 변수 선언문 
+	
+	printf("나이가 어떻게 되세요? : ");	scanf("%d", &age);
+	printf("점수는요? : ");	scanf("%d", &score);
+	
+	printf("\n\nMy age : %d \n", age);
+	printf("%d is my point \n", score);
+	printf("Good \nmorning \neverybody\n");
+	//printf("My age : %d \n%d is my point \nGood \nmorning \neverybody\n", 20 ,100);
+
+	return 0;
+}
